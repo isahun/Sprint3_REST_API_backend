ad72312dacc25d5b0a0752dc9cf76191c926b5e3
Irene <isahun7@gmail.com>
2026-03-01 21:35:28 +0100

    docs: swagger configuration finished

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configuració de la ValidationPipe globalment per a tots els DTOs
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina propietats no definides en el DTO
+      forbidNonWhitelisted: true, // Llença un error si hi ha propietats no definides
+      transform: true, // Transforma automàticament els tipus dels DTOs
+    }),
+  );
+
+  // Configuració de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API de Llibres')
+    .setDescription(
+      'Una API REST per gestionar un catàleg de llibres amb NestJS i MongoDB',
+    )
+    .setVersion('1.0')
+    .addTag('Llibres', 'Gestió del catàleg de llibres') // Afegim el tag que usem al controller
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // La documentació estarà disponible a /api-docs
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => {
+    console.log(`Servidor NestJS funcionant al port ${PORT}`);
+    console.log(
+      `Documentació Swagger disponible a http://localhost:${PORT}/api-docs`,
+    );
+  });
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error("Error durant l'arrencada del servidor:", err);
+});
