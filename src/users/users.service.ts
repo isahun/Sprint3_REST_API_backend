0d845c912f83557f7483f6718bd5a6a1648c3ee9
Irene Virgínia <isahun7@gmail.com>
2026-03-04 18:04:26 +0100

    feat: create users module with mongoose schema and bcrypt hashing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async findOne(username: string): Promise<User | null> {
+    return this.userModel.findOne({ username }).exec();
+  }
+
+  async create(username: string, passwordPlain: string): Promise<User> {
+    const existingUser = await this.userModel.findOne({ username }).exec();
+    if (existingUser) {
+      throw new ConflictException("Aquest nom d'usuari ja existeix.");
+    }
+    const hashedPassword = await bcrypt.hash(passwordPlain, 10); // 10 jump rounds
+    const newUser = new this.userModel({ username, password: hashedPassword });
+    return newUser.save();
+  }
+
+  async validatePassword(
+    passwordPlain: string,
+    hashedPasswordFromDb: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(passwordPlain, hashedPasswordFromDb);
+  }
+}
