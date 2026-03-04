d37e92f442db866a8988c3ac25cabce8e40ad9d4
Irene Virgínia <isahun7@gmail.com>
2026-03-04 18:27:42 +0100

    refactor: remove duplicate imports in appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config'; // Per gestionar variables d'entorn
 import { BooksModule } from './books/books.module';
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
