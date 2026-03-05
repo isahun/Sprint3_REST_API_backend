dafcbdc133a264c4142327429065f69de234cf82
Irene Virgínia <isahun7@gmail.com>
2026-03-05 20:35:34 +0100

    style: clean unused import in auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  UnauthorizedException,
-  ConflictException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { RegisterUserDto, LoginUserDto } from './dto/auth.dto';
